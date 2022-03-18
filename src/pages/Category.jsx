34bf7d7dafd3de8a7987631a9f3a66b84722b661
Aleksandra Marinovic <saska0799@gmail.com>
2022-03-18 21:57:52 +0100

    added form

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,7 +14,7 @@ import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -27,7 +27,7 @@ function Offers() {
 
         const q = query(
           listingsRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamp", "desc"),
           limit(10)
         );
@@ -50,11 +50,15 @@ function Offers() {
     };
 
     fetchListings();
-  });
+  }, [params.categoryName]);
   return (
     <div className="category">
       <header>
-        <p className="pageHeader">Offers</p>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Places for rent"
+            : "Places for sale"}
+        </p>
       </header>
       {loading ? (
         <Spinner />
@@ -73,10 +77,10 @@ function Offers() {
           </main>
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>No listings for {params.categoryName}</p>
       )}
     </div>
   );
 }
 
-export default Offers;
+export default Category;
